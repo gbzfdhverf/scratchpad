cfd044dc13598aa8e2b2e75c7afe7c44c72fb975
Tom Squires <tom@tomsquires.com>
2014-08-27 15:25:07 +0100

    Stack puzzles

---
+++ code-puzzles/src/main/java/com/tomsquires/puzzles/stacks/Stack.java
@@ -0,0 +1,39 @@
+package com.tomsquires.puzzles.stacks;
+
+import java.util.Arrays;
+
+public class Stack<T> {
+    private T elements[];
+    private int pointer;
+
+    public Stack() {
+        this.elements = (T[]) new Object[10];
+        this.pointer = 0;
+    }
+
+    public void push(T element) {
+        if (pointer + 1 > elements.length) {
+            elements = Arrays.copyOf(elements, elements.length * 2);
+        }
+        elements[pointer++] = element;
+    }
+
+    public T pop() {
+        T element = elements[pointer - 1];
+        elements[--pointer] = null;
+        return element;
+    }
+
+    public int size() {
+        return pointer;
+    }
+
+    public boolean peek(T element) {
+        for (int i = 0; i < pointer; ++i) {
+            if (elements[i].equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
